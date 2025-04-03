e82af26435784b371a44d53f1b5c66e8791d532a
David <29968843+power@users.noreply.github.com>
2025-04-03 10:03:28 +0100

    Added networking functionality for report generation

--- phanes.ps1
+++ phanes.ps1
@@ -5,17 +5,18 @@
 .DESCRIPTION
     Phanes is an educational tool designed to help individuals enhance their Active Directory knowledge. The tool intends to do this by generating a realistic & random network in real-time with common AD misconfigurations.
 
-    When using the -flags argument please use ONE of the following:
+    When using the -flags argument, please use ONE of the following:
     -All
-        - Generates the network with all features of Phanes
+        - Generates the network with all features of Phanes.
     -Users
         - Simply adds 20 users, no vulnerabilities present.
     -DC
-        - Adds vulnerabilities exclusively to DC01
+        - Adds vulnerabilities exclusively to DC01.
     -COMP
-        - Adds vulnerabilities exclusively to COMP01
+        - Adds vulnerabilities exclusively to COMP01.
 
-
+    Example Usage:
+    .\phanes.ps1 -dcip:"10.10.10.10" -path:"C:\Users\User\Desktop\Phanes" -flags:"-All:$true"
 .EXAMPLE
     PS> .\phanes.ps1 -dcip:"10.10.10.10" -path:"C:\Users\User\Desktop\Phanes" -flags:"-All:$true"
     
@@ -23,7 +24,7 @@
 .NOTES
     Author: David C
     Version: 1.0
-    Last Updated: 04/01/2025
+    Last Updated: 01/04/2025
 
 #>
 
@@ -42,27 +43,13 @@ function upload{
         Write-Host "[+] Flags was empty. Running with full functionality"
     }
 
-    # Define the share name and folder path
-    $ShareName = "Scripts"
-    $FolderPath = "$path"
-
-    # Set NTFS permissions to allow Everyone full control
-    $Acl = Get-Acl $FolderPath
-    $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
-    $Acl.SetAccessRule($AccessRule)
-    Set-Acl -Path $FolderPath -AclObject $Acl
-
-    # Create the SMB share and grant Everyone full access
-    New-SmbShare -Name $ShareName -Path $FolderPath -FullAccess Everyone
-
-    Write-Host "SMB share created"
     
-    #.\sysinternals\psexec.exe \\$DCIP -i -h -u "FAKECOMPANY.LOCAL\Administrator" -p "Admin123!" powershell -Command "cmd /c 'copy /y \\192.168.18.1\Scripts\upload\* C:\Users\Administrator\\Desktop\Scripts;ls'"
     .\sysinternals\psexec.exe \\$dcip -i -h -u "FAKECOMPANY.LOCAL\Administrator" -p "Admin123!" powershell -Command "Import-Module C:\Users\Administrator\Desktop\Scripts\ad_genUser.ps1; Invoke-ADGen $flags"
-    Remove-SmbShare -Name "Scripts" -Force 
+    .\sysinternals\psexec.exe \\$dcip -i -h -d -u "FAKECOMPANY.LOCAL\Administrator" -p "Admin123!" powershell -Command "cmd /c 'cd C:\Users\Administrator\Desktop\Scripts&ls&.\nc.exe -lvnp 9999 < vulns.json'"
+    cmd /c ".\nc.exe $dcip 9999 -w 3 > vulns.json"
+    python reportGen.py
     Write-Host "DONE"
     
 }
 
 upload
-#Write-Host $DCIP" + "$Path" + "$Flags
