997fb9d9efb6bb045fd2fd1a9c70afc15bc97834
David <29968843+power@users.noreply.github.com>
2025-04-08 11:27:26 +0100

    Refactor code to iterate through folders and provide full access, instead of access to the base folder

--- upload/badacl.ps1
+++ upload/badacl.ps1
@@ -7,15 +7,22 @@ function randomUserGen($temp_accounts) {
 function badAcl {
     $temp_accounts = [System.Collections.Generic.List[System.Object]](Get-ADGroupMember "IT Department" | Select-Object name)
     $it_users = $temp_accounts -replace '@{name=', '' -replace '}', ''
-    $file_dest = "C:\\Users\\Administrator"
-    # define our variables
-    $old_acl = Get-Acl -Path $file_dest
     $user = randomUserGen($it_users)
     $user = "FAKECOMPANY.LOCAL\$user"
-    # generate necessary information
+    $base_path = "C:\\Users\\Administrator"
+    $ItemList = Get-ChildItem -Path $base_path -Recurse;
     $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($user,"FullControl","Allow") #create the rule to give the user full control over the object
+    
+    
+    foreach ($Item in $ItemList) {
+        $Acl = Get-Acl -Path $Item.FullName; # Get the ACL from the item
+        $Acl.SetAccessRule($AccessRule); # Update the in-memory ACL
+        $Acl | Set-Acl $Item.FullName;  # Set the updated ACL on the target item
+    }
+    # define our variables
+    $old_acl = Get-Acl -Path $base_path
     $old_acl.SetAccessRule($AccessRule) # set the rule inside of the ACL
-    $old_acl | Set-Acl $file_dest # set the ACL
+    $old_acl | Set-Acl $base_path # set the ACL
 }
 
 badAcl
